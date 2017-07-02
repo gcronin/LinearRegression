78dcec53bdc5ee89db81d99b698cf464ca77fd25
gcronin <gcronin@seattleacademy.org>
2017-07-01 17:50:16 -0700

    Cleaned up "vocab" in script

--- LMSscript.py
+++ LMSscript.py
@@ -1,12 +1,12 @@
 import csv
 from random import randint
 
-sign_alphaSlope = 1           #incrementing or decrementing slope change
-sign_alphaIntercept = 1       #incrementing or decrementing intercept change
+sign_deltaSlope = 1           #incrementing or decrementing slope change
+sign_deltaIntercept = 1       #incrementing or decrementing intercept change
 slope = 250                   #initial guess
 intercept = 0                 #initial guess
-alphaSlope = 1                #how much does slope change with each iteration
-alphaIntercept = 1            #how much does intercept change with each iteration
+deltaSlope = 1                #how much does slope change with each iteration
+deltaIntercept = 1            #how much does intercept change with each iteration
 cost = 0                      #sum of squares of differences between predicted and actual data
 previousCost = 0              #stores cost from previous iteration so we can see if cost is decreasing
 
@@ -20,30 +20,30 @@ def CalculateCost():
     global cost
     previousCost = cost
     cost = 0
-    for x in range(0, len(data)):
-        cost += ((slope*data[x][0] + intercept) - data[x][1])**2
+    for i in range(0, len(data)):
+        cost += ((slope*data[i][0] + intercept) - data[i][1])**2
 
 def LinearRegression():
     global slope
     global intercept
-    global sign_alphaSlope
-    global sign_alphaIntercept
-    global alphaSlope
-    global alphaIntercept
+    global sign_deltaSlope
+    global sign_deltaIntercept
+    global deltaSlope
+    global deltaIntercept
     global cost
     global previousCost
     if(randint(0,1)):
-        slope += sign_alphaSlope*alphaSlope
+        slope += sign_deltaSlope*deltaSlope
         changeSlope = True
     else:
-        intercept += sign_alphaIntercept*alphaIntercept
+        intercept += sign_deltaIntercept*deltaIntercept
         changeSlope = False
     CalculateCost()
     if(cost > previousCost):
         if(changeSlope):
-            sign_alphaSlope *= -1
+            sign_deltaSlope *= -1
         else:
-            sign_alphaIntercept *= -1
+            sign_deltaIntercept *= -1
 
 
 
